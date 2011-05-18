b59f7c787f71ea3c1a494a68ff1b98dee8c5d7c2
David Yeung <yeungda@gmail.com>
2011-05-18 20:50:14 +1000

    added tomcat setup, war installation and cruise config.

--- puppet/files/deploy.rb
+++ puppet/files/deploy.rb
@@ -11,7 +11,11 @@ module MCollective
 
       action "war" do
         validate :source, String
-
+        run("wget -O /tmp/war #{request[:source]}")
+        run("service tomcat6 stop")
+        run("rm -r /var/lib/tomcat6/webapps/*")
+        run("mv /tmp/war /var/lib/tomcat6/webapps/companynews.war")
+        run("service tomcat6 start")
       end
     end
   end
--- puppet/manifests/app.pp
+++ puppet/manifests/app.pp
@@ -2,6 +2,32 @@ import "base.pp"
 
 class app inherits base {
 
+  $prevayler_path = '/var/db/pravaylor'
+
+  package { "tomcat6":
+    ensure   => "present",
+  }
+
+  file { '/var/db/pravaylor':
+    ensure => directory,
+    owner => 'tomcat',
+    group => 'tomcat',
+    require => Package['tomcat6'],
+  }
+
+  file { '/etc/tomcat6/tomcat6.conf':
+    content => template('tomcat6.conf.erb'),
+    owner => "tomcat",
+    group => "tomcat",
+    require => Package['tomcat6'],
+#    notify => Service['tomcat6']
+  }
+
+  service { "tomcat6":
+    ensure  => "running",
+    require => [Package['tomcat6'], File['/var/db/pravaylor'], File['/etc/tomcat6/tomcat6.conf']],
+  }
+ 
 }
 
 include app
